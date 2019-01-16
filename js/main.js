501514a720514bb5484c6800340fbe97ba36960c
Priyanka Saggu <priyankasggu11929@gmail.com>
2019-01-17 00:36:03 +0530

    Tried to add touch pointers

--- js/main.js
+++ js/main.js
@@ -164,6 +164,36 @@ Spider.prototype.die = function () {
 // Loading state
 // =============================================================================
 
+function create() {
+
+
+    //  By default Phaser only starts 2 pointers (enough for 2 fingers at once)
+
+    //  addPointer tells Phaser to add another pointer to Input, so here we are basically saying "allow up to 6 pointers + the mouse"
+
+    //  Note: on iOS as soon as you use 6 fingers you'll active the minimise app gesture - and there's nothing we can do to stop that, sorry
+
+    game.input.addPointer();
+    game.input.addPointer();
+    game.input.addPointer();
+    game.input.addPointer();
+
+}
+
+function render() {
+
+    //  Just renders out the pointer data when you touch the canvas
+    game.debug.pointer(game.input.mousePointer);
+    game.debug.pointer(game.input.pointer1);
+    game.debug.pointer(game.input.pointer2);
+    game.debug.pointer(game.input.pointer3);
+    game.debug.pointer(game.input.pointer4);
+    game.debug.pointer(game.input.pointer5);
+    game.debug.pointer(game.input.pointer6);
+
+}
+
+
 LoadingState = {};
 
 LoadingState.init = function () {
@@ -491,7 +521,7 @@ PlayState._createHud = function () {
 // =============================================================================
 
 window.onload = function () {
-    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+    let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game',  { create: create, render: render });
     game.state.add('play', PlayState);
     game.state.add('loading', LoadingState);
     game.state.start('loading');
